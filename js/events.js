76800a00dc804dee6a0572b5b4d809ac7eb129ea
Sarah Mellor <sarahmellor16@gmail.com>
2018-06-30 21:32:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,7 @@ function frameIt(){
 }
 function pressIt(){
   $("input").on('keydown',function(key){
-    if(key.which -== 71){alert('g was pressed');
+    if(key.which == 71){alert('g was pressed');
     }});
   })
 }
