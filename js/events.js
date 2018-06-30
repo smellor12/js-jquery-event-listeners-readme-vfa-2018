41416dbf218b83382f16af5aed614846b9d0cec5
Sarah Mellor <sarahmellor16@gmail.com>
2018-06-30 21:34:54 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
 
-$(document).ready(function(){
+
+$(document).ready(function(getIt){
 
 // call functions here
 
 });
+
+function getIt(){
+  $("p").on("click",function(){
+    window.alert("Hey!");
+  })
+}
+function frameIt(){
+  $("img").on("load",function(){
+    $("img").addClass("tasty");
+  })
+}
+function pressIt(){
+  $("input").on('keydown',function(key){
+    if(key.which == 71){alert('g was pressed');
+    }});
+}
+function submitIt(){
+  $("form").on("submit",function(){
+    window.alert("Your form is going to be submitted now.");
+  })
+}
