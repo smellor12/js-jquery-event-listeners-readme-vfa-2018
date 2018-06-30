c9c57f20af885f12e6038fc9b0f72ae03ea79f8f
Sarah Mellor <sarahmellor16@gmail.com>
2018-06-30 21:33:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,5 +21,9 @@ function pressIt(){
   $("input").on('keydown',function(key){
     if(key.which == 71){alert('g was pressed');
     }});
+}
+function submitIt(){
+  $("form").on("submit",function(){
+    window.alert("Your form is going to be submitted now.");
   })
 }
